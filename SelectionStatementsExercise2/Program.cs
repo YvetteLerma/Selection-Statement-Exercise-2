365eb8e8e79a6665e49b8a9b7a1492c26de8e871
Yvette Lerma <lerma.yvette@hotmail.com>
2022-10-20 08:47:49 -0600

    code has been added

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            //Switch Case
+
+
+            Console.WriteLine("What is your favorite school subject?");
+            string favSubject = Console.ReadLine();
+
+            switch (favSubject.ToLower())
+            {
+                case "math":
+                case "mathematics":
+                    Console.WriteLine("Math is such a tough and rewarding subject!");
+                    break;
+                case "writing":
+                case "language arts":
+                    Console.WriteLine("Writing and language can be difficult, but it is appreciated");
+                    break;
+                case "music":
+                case "band":
+                    Console.WriteLine("I have always wanted to learn how to play an instrument, so thats cool.");
+                    break;
+                case "gym":
+                    Console.WriteLine("Gym is a subject?");
+                    break;
+                case "art":
+                case "painting":
+                case "drawing":
+                    Console.WriteLine("I love art. I could get lost there.");
+                    break;
+                default:
+                    Console.WriteLine("Oh that sounds interesting.");
+                    break;
+            }
+            }
     }
 }
